69ee661d9194344ba721a02fd5d3e96413789c49
TeddyGavi <matc.davis@gmail.com>
2024-02-06 22:12:06 -0800

    turns out its not so easy to get input from a user in c

---
+++ hello_int.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int num;
+  printf("Type a int\n");
+  scanf("%i", &num);
+  printf("Your integer is %i \n", num);
+  return 0;
+}
--- hello_number.c
+++ hello_number.c
@@ -2,13 +2,10 @@
 
 int main(void)
 {
-  char name[25];
   int num;
   printf("Type a number: \n");
   scanf("%d", &num);
   printf("Your number is: %d", num);
 
-  scanf("%s", &name);
-  printf("Your name is: %s", name);
   return 0;
 }
