dbbc85c9feddb2015a64369edb9b763e792dc517
TeddyGavi <matc.davis@gmail.com>
2024-02-28 21:03:02 -0800

    with loop

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void)
-{
+int main(void) {
+  int i;
   printf("hello world\n");
+  for (i = 0; i < 3; i++) {
+    printf("hi from loop\n");
+  }
 }
